36bff5dfb706e1d21187d25919a6cdb7ed93e5d0
JWahle <jwahle@github.com>
2024-11-18 23:17:51 +0100

    Extracted BUILD_DIR variable and moving files to BUILD_DIR now happens each specific component's section

--- install_cdsp.sh
+++ install_cdsp.sh
@@ -6,6 +6,15 @@ CAMILLA_GUI_VERSION="v3.0.0-beta.4"
 PYCDSP_VERSION="next30"
 PYCDSP_PLOT_VERSION="next30"
 
+BUILD_DIR="/tmp/piCoreCDSP"
+
+### Decide for 64bit or 32bit installation
+if [ "aarch64" = "$(uname -m)" ]; then
+    use32bit=false
+else
+    use32bit=true
+fi
+
 ### Exit, if not enough free space
 requiredSpaceInMB=100
 availableSpaceInMB=$(/bin/df -m /dev/mmcblk0p2 | awk 'NR==2 { print $4 }')
@@ -22,18 +31,12 @@ if [ -f "/etc/sysconfig/tcedir/optional/piCoreCDSP.tcz" ]; then
     exit 1
 fi
 
-### Decide for 64bit or 32bit installation
-if [ "aarch64" = "$(uname -m)" ]; then
-    use32bit=false
-else
-    use32bit=true
-fi
-
-if [ -d "/tmp/piCoreCDSP" ]; then
+### Ensure fresh build dir exists
+if [ -d $BUILD_DIR ]; then
     >&2 echo "Reboot before running the script again."
     exit 1
 fi
-mkdir -p /tmp/piCoreCDSP
+mkdir -p $BUILD_DIR
 
 # Installs a module from the piCorePlayer repository - if not already installed.
 # Call like this: install_if_missing module_name
@@ -53,15 +56,14 @@ install_temporarily_if_missing(){
 
 set -v
 
-### Create CamillaDSP config folders
+
+### Creating CDSP data folders with default configuration
 
 cd /mnt/mmcblk0p2/tce
 mkdir -p camilladsp/configs
 mkdir -p camilladsp/coeffs
-
-### Create default config
-
 cd /mnt/mmcblk0p2/tce/camilladsp
+
 echo '
 devices:
   samplerate: 44100
@@ -95,14 +97,14 @@ volume:
 - 0.0
 ' > camilladsp_statefile.yml
 
-### Install ALSA CDSP
 
-cd /tmp
+### Building ALSA CDSP plugin
 
 install_temporarily_if_missing git
 install_temporarily_if_missing compiletc
 install_temporarily_if_missing libasound-dev
 
+cd /tmp
 git clone https://github.com/spfenwick/alsa_cdsp.git
 cd /tmp/alsa_cdsp
 git checkout $ALSA_CDSP_VERSION
@@ -110,18 +112,27 @@ make
 sudo make install
 cd /tmp
 rm -rf alsa_cdsp/
-cd /tmp
+
+cd $BUILD_DIR
+mkdir -p usr/local/lib/alsa-lib/
+sudo mv /usr/local/lib/alsa-lib/libasound_module_pcm_cdsp.so usr/local/lib/alsa-lib/libasound_module_pcm_cdsp.so
+
+
+### Installing ALSA CDSP config
 
 sudo chmod 664 /etc/asound.conf
 sudo chown root:staff /etc/asound.conf
+
 # Remove old configuration, in case it was installed before
 cat /etc/asound.conf |\
  tr '\n' '\r' |\
   sed 's|\r\r# For more info about this configuration see: https://github.com/scripple/alsa_cdsp\rpcm.camilladsp.*\r}\r# pcm.camilladsp||' |\
+  sed 's|\r\r# For more info about this configuration see: https://github.com/spfenwick/alsa_cdsp\rpcm.camilladsp.*\r}\r# pcm.camilladsp||' |\
    tr '\r' '\n' > /tmp/asound.conf
 cat /tmp/asound.conf > /etc/asound.conf
+
 echo '
-# For more info about this configuration see: https://github.com/scripple/alsa_cdsp
+# For more info about this configuration see: https://github.com/spfenwick/alsa_cdsp
 pcm.camilladsp {
     type cdsp
     cpath "/usr/local/camilladsp"
@@ -155,12 +166,28 @@ pcm.camilladsp {
 # pcm.camilladsp
 ' >> /etc/asound.conf
 
-### Set Squeezelite and Shairport output to camilladsp
+
+### Set Squeezelite and Shairport output to CamillaDSP
 
 sed 's/^OUTPUT=.*/OUTPUT="camilladsp"/' -i /usr/local/etc/pcp/pcp.cfg
 sed 's/^SHAIRPORT_OUT=.*/SHAIRPORT_OUT="camilladsp"/' -i /usr/local/etc/pcp/pcp.cfg
 
-### Install CamillaGUI
+
+### Downloading CamillaDSP
+
+mkdir -p ${BUILD_DIR}/usr/local/
+cd ${BUILD_DIR}/usr/local/
+if $use32bit; then
+    CDSP_URL=https://github.com/HEnquist/camilladsp/releases/download/${CDSP_VERSION}/camilladsp-linux-armv7.tar.gz
+else
+    CDSP_URL=https://github.com/HEnquist/camilladsp/releases/download/${CDSP_VERSION}/camilladsp-linux-aarch64.tar.gz
+fi
+wget -O camilladsp.tar.gz $CDSP_URL
+tar -xvf camilladsp.tar.gz
+rm -f camilladsp.tar.gz
+
+
+### Building CamillaGUI
 
 install_if_missing python3.11
 install_temporarily_if_missing python3.11-pip
@@ -181,7 +208,6 @@ wget https://github.com/HEnquist/camillagui-backend/releases/download/${CAMILLA_
 unzip camillagui.zip
 rm -f camillagui.zip
 echo '
----
 camilla_host: "0.0.0.0"
 camilla_port: 1234
 bind_address: "0.0.0.0"
@@ -199,39 +225,23 @@ on_get_active_config: null
 supported_capture_types: ["Stdin", "Alsa"]
 supported_playback_types: ["Alsa"]
 ' > config/camillagui.yml
+mkdir -p ${BUILD_DIR}/usr/local/
+sudo mv /usr/local/camillagui ${BUILD_DIR}/usr/local/
 
-touch /mnt/mmcblk0p2/tce/camilladsp/camilladsp_statefile.yml
 
-### Create and install piCoreCDSP.tcz
+### Creating autorun script
 
-mkdir -p /tmp/piCoreCDSP/usr/local/
-
-cd /tmp/piCoreCDSP/usr/local/
-
-if $use32bit; then
-    wget https://github.com/HEnquist/camilladsp/releases/download/${CDSP_VERSION}/camilladsp-linux-armv7.tar.gz
-    tar -xvf camilladsp-linux-armv7.tar.gz
-    rm -f camilladsp-linux-armv7.tar.gz
-else
-    wget https://github.com/HEnquist/camilladsp/releases/download/${CDSP_VERSION}/camilladsp-linux-aarch64.tar.gz
-    tar -xvf camilladsp-linux-aarch64.tar.gz
-    rm -f camilladsp-linux-aarch64.tar.gz
-fi
-
-cd /tmp/piCoreCDSP/
-
-mkdir -p usr/local/lib/alsa-lib/
-sudo mv /usr/local/lib/alsa-lib/libasound_module_pcm_cdsp.so usr/local/lib/alsa-lib/libasound_module_pcm_cdsp.so
-
-sudo mv /usr/local/camillagui usr/local/
-
-mkdir -p usr/local/tce.installed/
+mkdir -p ${BUILD_DIR}/usr/local/tce.installed/
+cd ${BUILD_DIR}/usr/local/tce.installed/
 echo "#!/bin/sh
 
 sudo -u tc sh -c 'while [ ! -f /usr/local/bin/python3 ]; do sleep 1; done
 source /usr/local/camillagui/environment/bin/activate
-python3 /usr/local/camillagui/main.py &' &" > usr/local/tce.installed/piCoreCDSP
-chmod 775 usr/local/tce.installed/piCoreCDSP
+python3 /usr/local/camillagui/main.py &' &" > piCoreCDSP
+chmod 775 piCoreCDSP
+
+
+### Building and installing piCoreCDSP extension
 
 cd /tmp
 install_temporarily_if_missing squashfs-tools
@@ -240,7 +250,8 @@ mv -f piCoreCDSP.tcz /etc/sysconfig/tcedir/optional
 echo "python3.11.tcz" > /etc/sysconfig/tcedir/optional/piCoreCDSP.tcz.dep
 echo piCoreCDSP.tcz >> /etc/sysconfig/tcedir/onboot.lst
 
-### Save Changes
+
+### Saving changes and rebooting
 
 pcp backup
 pcp reboot
