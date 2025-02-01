40efe0cf3817b2d60230e0f734a50b75e82c7e3e
JWahle <jwahle@github.com>
2025-02-01 22:10:05 +0100

    first samplerate-auto-fix.py script

--- samplerate-auto-fix.py
+++ samplerate-auto-fix.py
@@ -1,31 +1,32 @@
-from typing import Generator
+import os
+import re
+import time
+from typing import Optional
 
-import evdev
 from camilladsp import CamillaClient, CamillaError
-from evdev import KeyEvent, InputEvent
-
 
 # find available input devices via: ls /dev/input/by-id/
 cdsp_ip = "127.0.0.1"
 cdsp_port = 1234
 
+
 def main():
     cdsp = CamillaClient(cdsp_ip, cdsp_port)
-    print("Listening for input events")
-    for event in listen_for_key_events():
+    print("Monitoring samplerate...")
+    while True:
+        connect_to_cdsp_if_necessary(cdsp)
         try:
-            connect_to_cdsp_if_necessary(cdsp)
-            execute_action_for_event(cdsp, event)
+            alsa_samplerate = get_alsa_samplerate()
+            cdsp_samplerate = get_cdsp_samplerate(cdsp)
+            print("CDSP samplerate: " + str(cdsp_samplerate))
+            print("ALSA samplerate: " + str(alsa_samplerate))
+            if alsa_samplerate != cdsp_samplerate:
+                apply_alsa_samplerate(cdsp, alsa_samplerate)
         except CamillaError:
             print("Could not connect to CamillaDSP")
         except Exception as e:
             print(e)
-
-
-def listen_for_key_events() -> Generator[InputEvent, None, None]:
-    remote_control = evdev.InputDevice(input_device)
-    remote_control.grab()
-    return remote_control.read_loop()
+        time.sleep(2)
 
 
 def connect_to_cdsp_if_necessary(cdsp: CamillaClient):
@@ -33,39 +34,23 @@ def connect_to_cdsp_if_necessary(cdsp: CamillaClient):
         cdsp.connect()
 
 
-def execute_action_for_event(cdsp: CamillaClient, event: InputEvent):
-    key_event = key_press_event_of(event)
-    if key_event:
-        print("=== New event ===")
-        print("Keycode = " + repr(key_event.keycode))
-        for (keycode, action) in actions:
-            if keycode == key_event.keycode:
-                action(cdsp)
+def get_cdsp_samplerate(cdsp: CamillaClient) -> Optional[int]:
+    return cdsp.rate.capture()
 
 
-def key_press_event_of(event: InputEvent) -> KeyEvent | None:
-    if event.type == evdev.ecodes.EV_KEY:
-        key_event = evdev.categorize(event)
-        if key_event.keystate == evdev.KeyEvent.key_down:
-            return key_event
-    return None
+def get_alsa_samplerate():
+    stream = os.popen('cat /proc/asound/UltraLitemk5/pcm0p/sub0/hw_params | grep "rate: "')
+    output = stream.read()
+    rate_output_row = re.search('rate: (\d+).*', output)
+    rate_string = rate_output_row.group(1)
+    return int(rate_string)
 
 
-def adjust_volume(cdsp: CamillaClient, step: float):
-    print("Adjust volume by " + str(step))
-    cdsp.volume.adjust_volume(0, step, -100, 0)
-
-
-def mute(cdsp: CamillaClient):
-    print("Toggling Mute")
-    cdsp.volume.toggle_mute(0)
-
-
-def set_config(cdsp: CamillaClient, config_name: str):
-    new_config = "/mnt/mmcblk0p2/tce/camilladsp/configs/" + config_name
-    print("Setting config: " + new_config)
-    cdsp.config.set_file_path(new_config)
-    cdsp.general.reload()
+def apply_alsa_samplerate(cdsp: CamillaClient, samplerate: int):
+    config = cdsp.config.active()
+    config['devices']['capture_samplerate'] = samplerate
+    cdsp.config.set_active(config)
+    print("Updated config with samplerate " + str(samplerate))
 
 
 if __name__ == '__main__':
